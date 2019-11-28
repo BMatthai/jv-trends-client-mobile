4f9b3ef42094696f5266e36400d428f93485df4f
Bastien MATTHÄI <bastien_matthai1@hotmail.fr>
2019-11-29 00:48:50 +0100

    first render

--- jv-trends-mobile/App.js
+++ jv-trends-mobile/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 class App extends Component {
 
@@ -16,25 +18,61 @@ class App extends Component {
   }
 
   fetchTrends() {
-    return fetch('http://192.168.0.13:5000/trends?top=5&interval=60')
+    data =fetch('http://192.168.0.13:5000/trends?top=5&interval=60')
     .then((response) => response.json())
     .then((data) => this.setState({ topics: data.topics }))
     .catch((error) => {
     console.error(error);
     });
+
+    return data
   }
 
   render() {
-    console.log(this.state.topics)
-    const items = this.state.topics.map((item, key) =>
-       <Text>{item.title}</Text>
-       <Text>{item.oldval}</Text>
-       <Text>{item.newval}</Text>
+    const state = this.state
+    
+    array_topics = []
+    for (let i = 0; i < state.topics.length; i += 1) {
+      cur = state.topics[i]
+      array_topics.push([cur.title, cur.delta, cur.oldval, cur.newval])
+    }
 
-    );
-    return items;
-
+       return (
+      <View style={styles.container}>
+        <ScrollView horizontal={true}>
+          <View>
+            <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+              <Row data={state.tableHead} widthArr={state.widthArr} style={styles.header} textStyle={styles.text}/>
+            </Table>
+            <ScrollView style={styles.dataWrapper}>
+              <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+                {
+                  array_topics.map((rowData, index) => (
+                    <Row
+                      key={index}
+                      data={rowData}
+                      widthArr={state.widthArr}
+                      style={[styles.row, index%2 && {backgroundColor: '#F7F6E7'}]}
+                      textStyle={styles.text}
+                    />
+                  ))
+                }
+              </Table>
+            </ScrollView>
+          </View>
+        </ScrollView>
+      </View>
+    )
   }
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  head: {  height: 40,  backgroundColor: '#f1f8ff'  },
+  wrapper: { flexDirection: 'row' },
+  title: { flex: 1, backgroundColor: '#f6f8fa' },
+  row: {  height: 28  },
+  text: { textAlign: 'center' }
+});
 
 export default App;
